0b3eddc462eb4c793df83637e3c3ffd045824509
Neil M <neilm813@gmail.com>
2022-07-27 12:42:23 -0500

    rm eslint, add folders

--- api/src/configs/mongoose-config.js
+++ api/src/configs/mongoose-config.js
@@ -1,32 +1,15 @@
 const mongoose = require('mongoose');
 
 // Export a function to be called from index.js
-module.exports = (db_name) => {
-  console.log(`Starting connection to ${db_name}.`);
+module.exports = (dbName) => {
+  console.log(`Starting connection to ${dbName}.`);
   mongoose
-    // ip address instead of localhost for windows issue
-    // .connect(`mongodb://localhost/${db_name}`, {
-    .connect(`mongodb://127.0.0.1:27017/${db_name}`, {
+    .connect(`mongodb://127.0.0.1:27017/${dbName}`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
     .then(() => {
-      console.log(`Successfully connected to ${db_name}`);
+      console.log(`Successfully connected to ${dbName}`);
     })
-    .catch((err) =>
-      console.log(`mongoose connection to ${db_name} failed:`, err)
-    );
+    .catch((err) => console.log(`mongoose connection to ${dbName} failed:`, err));
 };
-
-// This also works
-// mongoose.connect(`mongodb://localhost/${db_name}`, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   /*
-//     WINDOWS:
-//     Below opt needed for MongoDB to work as a Windows service for me.
-//     Alternatively, this terminal cmd can be used: mongod --ipv6
-//     instead of a Windows service and the below opt.
-//     */
-//   family: 4,
-// });
---
+++ client/src/hooks/index.js
@@ -0,0 +1,8 @@
+/* 
+Shared custom hooks that multiple components could use.
+Custom hooks that are only for a specific component can be placed in that
+component's folder instead.
+*/
+
+export { useLocalStorage } from './useLocalStorage';
+export { usePromise } from './usePromise';
