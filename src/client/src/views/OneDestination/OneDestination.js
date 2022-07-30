08cb2610f9c493c7ae3279c36aa46e18df808676
Neil M <neilm813@gmail.com>
2022-07-30 01:00:42 -0500

    add react-query and initial OneDestination view

--- src/client/src/views/OneDestination/OneDestination.js
+++ src/client/src/views/OneDestination/OneDestination.js
@@ -1,3 +1,49 @@
+import { useParams, Link } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  CircularProgress,
+  Fade,
+  Paper,
+  Typography,
+} from '@mui/material';
+
+import { getOneDestination } from 'services';
+
 export const OneDestination = (_props) => {
-  return "OneDestination View";
+  const { id } = useParams();
+  const {
+    data: destination,
+    isLoading,
+    isError,
+    error,
+  } = useQuery(['one-destination'], () => getOneDestination(id));
+
+  if (error) {
+    return 'error';
+  }
+
+  return (
+    <Box
+      sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      {isLoading && <CircularProgress />}
+      {destination && (
+        <Card>
+          <CardMedia
+            component="img"
+            image={destination.src}
+            alt={destination.location}
+          />
+          <CardContent>
+            <Typography variant="h4">{destination.location}</Typography>
+            <Typography variant="body2">{destination.description}</Typography>
+          </CardContent>
+        </Card>
+      )}
+    </Box>
+  );
 };
