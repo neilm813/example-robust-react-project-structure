97d59fa03fa543c46d9f9163a3f0c39f1a6a5aaf
Neil M <neilm813@gmail.com>
2022-07-30 20:53:55 -0500

    constants example

--- src/client/src/theme.js
+++ src/client/src/theme.js
@@ -1,7 +1,8 @@
 import { createTheme } from '@mui/material/styles';
 
 /**
- * @see [MUI Theming](https://mui.com/material-ui/customization/theming/)
+ * - @see [MUI Theming](https://mui.com/material-ui/customization/theming/)
+ * - @see [MUI Palette Colors](https://mui.com/material-ui/customization/palette/#default-values)
  */
 export const theme = createTheme({
   palette: {
