447b6c586af26290caf171642d4bffe30f382af8
Neil M <neilm813@gmail.com>
2022-07-31 17:00:58 -0500

    add sleep method

---
+++ src/client/src/utils/timing.js
@@ -0,0 +1,15 @@
+/**
+ * Pauses execution for the given amount of milliseconds.
+ * @param {number} ms The milliseconds to wait before next line of code runs.
+ * @example
+ * ```
+ * // Waits 1 second before next line runs.
+ * await sleep(1000);
+ * ```
+ */
+export const sleep = async (ms) =>
+  new Promise((resolve) =>
+    setTimeout(() => {
+      resolve();
+    }, ms)
+  );
