9e608ca487dff1277be2297f0a2d6f29699acc1a
Neil M <plan.with.neil@gmail.com>
2023-03-27 17:33:02 -0500

    validation messages

--- apps/api/src/models/destinationModel.js
+++ apps/api/src/models/destinationModel.js
@@ -4,46 +4,45 @@ const DestinationSchema = new mongoose.Schema(
   {
     location: {
       type: String,
-      required: [true, 'is required.'],
-      minlength: [2, 'must be at least {MINLENGTH} characters.'],
+      required: [true, 'required field'],
+      minlength: [2, '{MINLENGTH} characters required'],
     },
     description: {
       type: String,
-      required: [true, 'is required.'],
-      minlength: [5, 'must be at least {MINLENGTH} characters.'],
+      required: [true, 'required field'],
+      minlength: [5, '{MINLENGTH} characters required'],
     },
     src: {
       type: String,
-      required: [true, 'is required.'],
+      required: [true, 'required field'],
     },
     srcType: {
       type: String,
-      required: [true, 'is required.'],
+      required: [true, 'required field'],
     },
-    // Checkboxes for the season's you'd like to travel to this destination.
+    // Checkboxes for the season's you'd like to travel to this destination
+
     summer: {
       type: Boolean,
       default: false,
     },
+
     winter: {
       type: Boolean,
       default: false,
     },
+
     spring: {
       type: Boolean,
       default: false,
     },
+
     fall: {
       type: Boolean,
       default: false,
     },
-    // population: {
-    //   type: Number,
-    //   required: [true, "{PATH} is required."],
-    //   min: [0, "{PATH} must be {MIN} at minimum."],
-    // },
   },
-  { timestamps: true } // adds createdAt and updatedAt.
+  { timestamps: true }
 );
 
 export const Destination = mongoose.model('Destination', DestinationSchema);
