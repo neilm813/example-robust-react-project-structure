cc00709c8b6271a68a088f96b90e8255d82339b4
Neil M <plan.with.neil@gmail.com>
2023-03-27 17:48:24 -0500

    add eslint import organization rules

--- apps/client/src/views/NewDestinationView/NewDestinationView.js
+++ apps/client/src/views/NewDestinationView/NewDestinationView.js
@@ -1,1 +1,7 @@
-export const NewDestinationView = (_props) => 'NewDestinationView';
+import { Box, FormGroup, Input, Paper } from '@mui/material';
+
+export const NewDestinationView = (_props) => (
+  <Paper elevation={3}>
+    <form>sup</form>
+  </Paper>
+);
